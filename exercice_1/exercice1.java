9fe411b4d813228034870080d7e9c3b151b86b75
MEIKEU <meikeutg@3il.fr>
2024-11-26 14:56:10 +0100

    restructuration de la fonction calculerfacture presente dans l'exercice1.java

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,23 +1,40 @@
+
 public class FactureManager {
-
-    // Calcul le total d'une facture
+    //class permettant le calcul des facture
+    /**
+     * calcule la facture totale avec reduction
+     * @param typeProduit 
+     * @param quantite 
+     * @param prixUnitaire 
+     * @return retourne le total de la facture avec la reduction
+     */
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
-        double total = quantite * prixUnitaire;
-
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
+        double resultat = calculTotal(quantite,prixUnitaire)(1-reduction(typeProduit));
+         if ( resultat> 1000) {
+          return  resultat*0.95; 
         }
-
-        // Reduction sur le total
-        if (total > 1000) {
-            total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
-        }
-
-        return total;
+        return resultat;    
     }
+    /**
+     * clacule le total sans reduction
+     * @return retourne le total sans reduction
+     * @param quantite 
+     * @param prixUnitaire 
+     */
+    public double calculTotal(int quantite, double prixUnitaire){
+        return quantite*prixUnitaire;
+    }
+    /**
+     * @return 
+     * @param typeProduit
+     */
+    public double reduction(double typeProduit){
+        switch(typeProduit){
+            case "Alimentaire":return 0.05;break;
+            case "Electronique":return 0.1;break;
+            case "Luxe": return 0.15;break;
+            default : return 0;break;
+        }
+    }
+      
 }
